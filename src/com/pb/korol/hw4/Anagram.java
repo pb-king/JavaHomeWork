b8df7d056bbe3337109a6cb5b8f59156c83b4bfe
dn140486kav <andrej.korol@privatbank.ua>
2021-10-24 14:21:50 +0300

    added homework 4

--- src/com/pb/korol/hw4/Anagram.java
+++ src/com/pb/korol/hw4/Anagram.java
@@ -1,7 +1,29 @@
 package com.pb.korol.hw4;
 
+import java.util.Scanner;
+
 public class Anagram {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String string1, string2, result = "Строка 2 ";
 
+        System.out.println("Введите строку 1:");
+        string1 = console.nextLine();
+        System.out.println("Введите строку 2:");
+        string2 = console.nextLine();
+
+        result += (isAnagram(string1, string2)) ? "- анаграмма" : "не является анаграммой" + " строки 1";
+        System.out.println(result);
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        // char[] letters1 = getLetters(str1), letters2 = getLetters(str2);
+
+        return false;
+    }
+
+    public static char[] getLetters(String string) {
+        char[] array = string.toLowerCase().toCharArray();
+        return array;
     }
 }
--- src/com/pb/korol/hw4/CapitalLetter.java
+++ src/com/pb/korol/hw4/CapitalLetter.java
@@ -1,7 +1,48 @@
 package com.pb.korol.hw4;
 
+import java.util.Scanner;
+
 public class CapitalLetter {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String inputString, outputString;
 
+        System.out.println("Введите строку для обработки:");
+        inputString = console.nextLine();
+        outputString = toProperCase(inputString);
+
+        System.out.println("Строка после обработки:");
+        System.out.println(outputString);
+    }
+
+    public static String toProperCase(String text) {
+        char[] textArray = text.toCharArray();
+        String prevType = "separator";
+
+        for (int i = 0; i < textArray.length; i++) {
+            char curChar = textArray[i];
+            String curType = charType(curChar);
+
+            if (curType.equals("lowercase") && prevType.equals("separator")) {
+                textArray[i] = Character.toUpperCase(curChar);
+            }
+            prevType = curType;
+        }
+
+        return String.valueOf(textArray);
+    }
+
+    public static String charType(char symbol) {
+        final String separators = " \t,.!?\"";
+
+        if (separators.indexOf(symbol) != -1) {
+            return "separator";
+        }
+        else if (Character.isLowerCase(symbol)) {
+            return "lowercase";
+        }
+        else {
+            return "other";
+        }
     }
 }
