8875aa13c6ec38363584ac6a195698a7c0b73098
dn140486kav <andrej.korol@privatbank.ua>
2021-10-25 14:14:40 +0300

    hw4 - updated Anagram

--- src/com/pb/korol/hw4/Anagram.java
+++ src/com/pb/korol/hw4/Anagram.java
@@ -1,29 +1,56 @@
 package com.pb.korol.hw4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Anagram {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        String string1, string2, result = "Строка 2 ";
+        String inputString1, inputString2, result = "Строка 2 ";
 
         System.out.println("Введите строку 1:");
-        string1 = console.nextLine();
+        inputString1 = console.nextLine();
         System.out.println("Введите строку 2:");
-        string2 = console.nextLine();
+        inputString2 = console.nextLine();
 
-        result += (isAnagram(string1, string2)) ? "- анаграмма" : "не является анаграммой" + " строки 1";
+        result += (isAnagram(inputString1, inputString2)) ? "- анаграмма" : "не является анаграммой";
+        result += " строки 1";
         System.out.println(result);
     }
 
-    public static boolean isAnagram(String str1, String str2) {
-        // char[] letters1 = getLetters(str1), letters2 = getLetters(str2);
-
-        return false;
+    public static boolean isAnagram(String string1, String string2) {
+        // анаграммы нечувствительны к регистру, поэтому приведем строки в lowercase
+        string1 = string1.toLowerCase();
+        string2 = string2.toLowerCase();
+        // сохраняем в ArrayList все буквы строки 1
+        ArrayList<Character> letters1 = getLetters(string1);
+        // перебираем все символы строки 2
+        for (int i = 0; i < string2.length(); i++) {
+            char symbol = string2.charAt(i);
+            // если встречена буква, ищем ее в списке букв строки 1
+            if (Character.isLetter(symbol)) {
+                int index = letters1.indexOf(symbol);
+                // если не нашли, сходу не анаграмма
+                if (index == -1) {
+                    return false;
+                } // иначе удаляем найденную букву из списка букв строки 1
+                else {
+                    letters1.remove(index);
+                }
+            }
+        }
+        // если после перебора строки 2 остались еще буквы строки 1 - это не анаграмма
+        return letters1.size() == 0;
     }
 
-    public static char[] getLetters(String string) {
-        char[] array = string.toLowerCase().toCharArray();
-        return array;
+    public static ArrayList<Character> getLetters(String str) {
+        ArrayList<Character> letters = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                letters.add(str.charAt(i));
+            }
+        }
+        return letters;
     }
 }
