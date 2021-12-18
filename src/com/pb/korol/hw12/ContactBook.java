a437e0d5eef32d3f3ab40f9a2b9c346d1373f872
dn140486kav <andrej.korol@privatbank.ua>
2021-12-18 20:35:23 +0200

    ContactBook update

--- src/com/pb/korol/hw12/ContactBook.java
+++ src/com/pb/korol/hw12/ContactBook.java
@@ -1,17 +1,7 @@
-package com.pb.korol.hw11;
+package com.pb.korol.hw12;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-// задача еще не готова полностью, извините
 
 public class ContactBook {
     public static void main(String[] args) throws JsonProcessingException {
@@ -36,5 +26,4 @@ public class ContactBook {
     public static void showMenu() {
         System.out.println("меню");
     }
-
 }
