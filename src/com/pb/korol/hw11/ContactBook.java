60753e4456a9c6703dfb828dc5b015efa4b19866
dn140486kav <andrej.korol@privatbank.ua>
2021-12-19 13:22:01 +0200

    ContactBook update 3

--- src/com/pb/korol/hw11/ContactBook.java
+++ src/com/pb/korol/hw11/ContactBook.java
@@ -3,6 +3,11 @@ package com.pb.korol.hw11;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,37 +18,9 @@ public class ContactBook {
         ObjectMapper objectMapper = new ObjectMapper();
         Contact contact = new Contact("Dude", "01.01.2000", "some street");
         Contact contact2 = new Contact("Chick", "02.02.2002", "next street");
-        int choice;
-        /*
         ContactList contacts = new ContactList();
-        contacts.add(contact);
-        contacts.add(contact2);
-
-        String result = objectMapper.writeValueAsString(contacts);
-        System.out.println(result);
-
-        ContactList contacts2 = objectMapper.readValue(result, ContactList.class);
-        contacts2.print();
-        System.out.println();
-        */
-        do {
-            choice = getMainMenuChoise();
-
-            switch (choice) {
-                case 0: addContact();
-                    break;
-                default:
-                    System.out.println("Неизвестный код меню!");
-            }
-            System.out.println("Вы выбрали вариант: " + choice);
-        }
-        while (choice == 0);
-    }
-
-    public static int getMainMenuChoise() {
-        Scanner console = new Scanner(System.in);
-        String choice;
-        String[] menuList = {
+        String filePath = "contacts.json";
+        String[] mainMenuText = {
                 "1 - добавить контакт",
                 "2 - найти контакт",
                 "3 - вывести все контакты",
@@ -51,10 +28,38 @@ public class ContactBook {
                 "5 - загрузить контакты из файла",
                 "0 - завершить работу"
         };
+        int usersChoice = 0;
+
+        do {
+            usersChoice = getMenuChoice(mainMenuText);
+
+            switch (usersChoice) {
+                case 1: addContact(contacts);
+                    break;
+                case 2: findContact(contacts);
+                    break;
+                case 3: showAllContacts(contacts);
+                    break;
+                case 4: saveContactsToFile(contacts, filePath);
+                    break;
+                case 5: loadContactsFromFile(contacts, filePath);
+                    break;
+                default:
+                    System.out.println("Неизвестный код меню!");
+            }
+            System.out.println("Вы выбрали вариант: " + usersChoice);
+        }
+        while (usersChoice != 0);
+    }
+
+    public static int getMenuChoice(String[] menuText) {
+        Scanner console = new Scanner(System.in);
+        String choice;
+
         ArrayList<String> possibleChoices = new ArrayList<>();
 
         System.out.println("Выберите действие из списка (введите цифру):");
-        for (String menuItem: menuList) {
+        for (String menuItem: menuText) {
             System.out.println(menuItem);
             possibleChoices.add(menuItem.substring(0, 1));
         }
@@ -67,8 +72,40 @@ public class ContactBook {
         return Integer.parseInt(choice);
     }
 
-    public static void addContact() {
+    public static void addContact(ContactList contacts) {
         Scanner console = new Scanner(System.in);
+        System.out.println("ввод нового контакта");
+    }
 
+    public static void findContact(ContactList contacts) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("поиск нового контакта");
+    }
+
+    public static void showAllContacts(ContactList contacts) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("вывод всех контактов");
+    }
+
+    public static void saveContactsToFile(ContactList contacts, String filePath) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonAsString = objectMapper.writeValueAsString(contacts);
+        Path path = Paths.get(filePath);
+
+        System.out.println(jsonAsString);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.write(jsonAsString);
+        } catch (IOException exWrite) {
+            System.out.println("Ошибка записи в файл " + filePath + ": " + exWrite);
+        }
+        System.out.println("Данные записаны в файл " + filePath + ".");
+    }
+
+    public static void loadContactsFromFile(ContactList contacts, String filePath) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ContactList contacts2 = objectMapper.readValue(result, ContactList.class);
+        contacts2.print();
+        System.out.println();
     }
 }
