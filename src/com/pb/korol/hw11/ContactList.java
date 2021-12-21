579b2eed378fe18abbc207ad85bc734106a414a1
dn140486kav <andrej.korol@privatbank.ua>
2021-12-21 22:11:15 +0200

    hw11 update 2

--- src/com/pb/korol/hw11/ContactList.java
+++ src/com/pb/korol/hw11/ContactList.java
@@ -9,16 +9,29 @@ public class ContactList {
         this.list = new ArrayList<>();
     }
 
-    ContactList(ArrayList<Contact> contacts) {
-        this.list = contacts;
-    }
-
     public void add(Contact contact) {
         list.add(contact);
     }
 
     public void del(int index) {
         list.remove(index);
+    }
+
+    public boolean del(Contact contact) {
+        int index = list.indexOf(contact);
+        if (index != -1) {
+            list.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public Contact get(int index) {
+        return list.get(index);
+    }
+
+    public int length() {
+        return list.size();
     }
 
     public ArrayList<Contact> getList() {
@@ -31,8 +44,48 @@ public class ContactList {
         }
         else {
             for (Contact contact: list) {
+                System.out.println();
+                System.out.println("Контакт № " + (list.indexOf(contact) + 1));
                 System.out.println(contact);
             }
         }
+    }
+
+    public Contact find(String key, String value) {
+        switch (key) {
+            case "number":
+                int index = Integer.parseInt(value) - 1;
+                if (index >= 0 && index < list.size()) {
+                    return list.get(index);
+                }
+                else {
+                    System.out.println("Введенное число за пределами списка!");
+                }
+            case "name":
+                for(Contact contact: list) {
+                    if (contact.getFullName().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "birth":
+                for(Contact contact: list) {
+                    if (contact.getBirthDate().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "address":
+                for(Contact contact: list) {
+                    if (contact.getAddress().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "phone":
+                for(Contact contact: list) {
+                    if (contact.getPhoneNumbers().contains(value)) {
+                        return contact;
+                    }
+                }
+        }
+        return null;
     }
 }
