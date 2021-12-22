6378c825862738a8d2471d84eff082cc51555efc
dn140486kav <andrej.korol@privatbank.ua>
2021-12-22 19:54:21 +0200

    hw12 update 1

--- src/com/pb/korol/hw12/Contact.java
+++ src/com/pb/korol/hw12/Contact.java
@@ -1,12 +1,15 @@
 package com.pb.korol.hw12;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Contact {
     private String fullName;
     private String birthDate;
     private String address;
-    private String[] phones;
+    private ArrayList<String> phoneNumbers;
+    private LocalDateTime changeTime;
 
     public Contact() {
     }
@@ -15,6 +18,8 @@ public class Contact {
         this.fullName = fullName;
         this.birthDate = birthDate;
         this.address = address;
+        this.phoneNumbers = new ArrayList<>();
+        this.changeTime = LocalDateTime.now();
     }
 
     public String getAddress() {
@@ -29,13 +34,46 @@ public class Contact {
         return fullName;
     }
 
+    public ArrayList<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public LocalDateTime getChangeTime() {
+        return changeTime;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+        this.changeTime = LocalDateTime.now();
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+        this.changeTime = LocalDateTime.now();
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+        this.changeTime = LocalDateTime.now();
+    }
+
+    public void addPhoneNumber(String phoneNumber) {
+        this.phoneNumbers.add(phoneNumber);
+        this.changeTime = LocalDateTime.now();
+    }
+
+    public void delPhoneNumber(int index) {
+        this.phoneNumbers.remove(index);
+        this.changeTime = LocalDateTime.now();
+    }
+
     @Override
     public String toString() {
-        return "{" +
-                "fullName='" + fullName + '\'' +
-                ", birthDate='" + birthDate + '\'' +
-                ", address='" + address + '\'' +
-                ", phones=" + Arrays.toString(phones) +
-                '}';
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        return "ФИО: " + fullName +
+               "\nДата рождения: " + birthDate +
+               "\nАдрес: " + address +
+               "\nПоследнее изменение: " + changeTime.format(formatter) +
+               "\nКонтактные номера: " + phoneNumbers;
     }
 }
--- src/com/pb/korol/hw12/ContactList.java
+++ src/com/pb/korol/hw12/ContactList.java
@@ -9,10 +9,6 @@ public class ContactList {
         this.list = new ArrayList<>();
     }
 
-    ContactList(ArrayList<Contact> contacts) {
-        this.list = contacts;
-    }
-
     public void add(Contact contact) {
         list.add(contact);
     }
@@ -21,13 +17,75 @@ public class ContactList {
         list.remove(index);
     }
 
+    public boolean del(Contact contact) {
+        int index = list.indexOf(contact);
+        if (index != -1) {
+            list.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public Contact get(int index) {
+        return list.get(index);
+    }
+
+    public int length() {
+        return list.size();
+    }
+
     public ArrayList<Contact> getList() {
         return list;
     }
 
     public void print() {
-        for(Contact contact: list) {
-            System.out.println(contact);
+        if (list.size() == 0) {
+            System.out.println("Список контактов пуст");
         }
+        else {
+            for (Contact contact: list) {
+                System.out.println();
+                System.out.println("Контакт № " + (list.indexOf(contact) + 1));
+                System.out.println(contact);
+            }
+        }
+    }
+
+    public Contact find(String key, String value) {
+        switch (key) {
+            case "number":
+                int index = Integer.parseInt(value) - 1;
+                if (index >= 0 && index < list.size()) {
+                    return list.get(index);
+                }
+                else {
+                    System.out.println("Введенное число за пределами списка!");
+                }
+            case "name":
+                for(Contact contact: list) {
+                    if (contact.getFullName().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "birth":
+                for(Contact contact: list) {
+                    if (contact.getBirthDate().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "address":
+                for(Contact contact: list) {
+                    if (contact.getAddress().equals(value)) {
+                        return contact;
+                    }
+                }
+            case "phone":
+                for(Contact contact: list) {
+                    if (contact.getPhoneNumbers().contains(value)) {
+                        return contact;
+                    }
+                }
+        }
+        return null;
     }
 }
