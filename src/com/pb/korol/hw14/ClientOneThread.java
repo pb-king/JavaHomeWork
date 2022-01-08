f485485b601821f447978fc8254ebfbc4fd020c2
dn140486kav <andrej.korol@privatbank.ua>
2022-01-08 16:49:53 +0200

    hw 14 update 1

--- src/com/pb/korol/hw14/ClientOneThread.java
+++ src/com/pb/korol/hw14/ClientOneThread.java
@@ -1,7 +1,31 @@
 package com.pb.korol.hw14;
 
-public class ClientOneThread {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientOneThread {
+    public static void main(String[] args) throws Exception {
+        int portNumber = 1777;
+        Socket socket = new Socket("127.0.0.1", portNumber);
+        System.out.println("Client is started");
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+        pw.println("Тестовая строка для передачи");
+
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            if (str.equals("exit")) {
+                break;
+            }
+            System.out.println(str);
+        }
+
+        br.close();
+        pw.close();
+        socket.close();
     }
 }
