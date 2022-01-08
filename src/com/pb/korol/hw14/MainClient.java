95a2b819812074211bec909db9ce00122ee6bb93
dn140486kav <andrej.korol@privatbank.ua>
2022-01-08 19:01:51 +0200

    hw 14 update 1

---
+++ src/com/pb/korol/hw14/MainClient.java
@@ -0,0 +1,8 @@
+package com.pb.korol.hw14;
+
+public class MainClient {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+        ClientWindow clientWindow2 = new ClientWindow();
+    }
+}
---
+++ src/com/pb/korol/hw14/MainServer.java
@@ -0,0 +1,7 @@
+package com.pb.korol.hw14;
+
+public class MainServer {
+    public static void main(String[] args) {
+        EchoServer server = new EchoServer();
+    }
+}
