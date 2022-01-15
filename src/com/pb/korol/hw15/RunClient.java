1708f35317f221e78081687efe7441235283042f
dn140486kav <andrej.korol@privatbank.ua>
2022-01-15 20:36:15 +0200

    added homework 15

---
+++ src/com/pb/korol/hw15/RunClient.java
@@ -0,0 +1,8 @@
+package com.pb.korol.hw15;
+
+public class RunClient {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+        ClientWindow clientWindow2 = new ClientWindow();
+    }
+}
---
+++ src/com/pb/korol/hw15/RunServer.java
@@ -0,0 +1,7 @@
+package com.pb.korol.hw15;
+
+public class RunServer {
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer();
+    }
+}
